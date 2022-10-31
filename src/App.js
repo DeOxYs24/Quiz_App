64d74a9913a95ff54f384f31e67c375f03265ac2
Saheel Kumar <71027342+DeOxYs24@users.noreply.github.com>
2022-10-31 16:30:55 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,44 +1,44 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function App() {
-	const questions = [
-		{
-			questionText: 'What is the capital of France?',
-			answerOptions: [
-				{ answerText: 'New York', isCorrect: false },
-				{ answerText: 'London', isCorrect: false },
-				{ answerText: 'Paris', isCorrect: true },
-				{ answerText: 'Dublin', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'Who is CEO of Tesla?',
-			answerOptions: [
-				{ answerText: 'Jeff Bezos', isCorrect: false },
-				{ answerText: 'Elon Musk', isCorrect: true },
-				{ answerText: 'Bill Gates', isCorrect: false },
-				{ answerText: 'Tony Stark', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'The iPhone was created by which company?',
-			answerOptions: [
-				{ answerText: 'Apple', isCorrect: true },
-				{ answerText: 'Intel', isCorrect: false },
-				{ answerText: 'Amazon', isCorrect: false },
-				{ answerText: 'Microsoft', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'How many Harry Potter books are there?',
-			answerOptions: [
-				{ answerText: '1', isCorrect: false },
-				{ answerText: '4', isCorrect: false },
-				{ answerText: '6', isCorrect: false },
-				{ answerText: '7', isCorrect: true },
-			],
-		},
-	];
+	// const questions = [
+	// 	{
+	// 		questionText: 'What is the capital of France?',
+	// 		answerOptions: [
+	// 			{ answerText: 'New York', isCorrect: false },
+	// 			{ answerText: 'London', isCorrect: false },
+	// 			{ answerText: 'Paris', isCorrect: true },
+	// 			{ answerText: 'Dublin', isCorrect: false },
+	// 		],
+	// 	},
+	// 	{
+	// 		questionText: 'Who is CEO of Tesla?',
+	// 		answerOptions: [
+	// 			{ answerText: 'Jeff Bezos', isCorrect: false },
+	// 			{ answerText: 'Elon Musk', isCorrect: true },
+	// 			{ answerText: 'Bill Gates', isCorrect: false },
+	// 			{ answerText: 'Tony Stark', isCorrect: false },
+	// 		],
+	// 	},
+	// 	{
+	// 		questionText: 'The iPhone was created by which company?',
+	// 		answerOptions: [
+	// 			{ answerText: 'Apple', isCorrect: true },
+	// 			{ answerText: 'Intel', isCorrect: false },
+	// 			{ answerText: 'Amazon', isCorrect: false },
+	// 			{ answerText: 'Microsoft', isCorrect: false },
+	// 		],
+	// 	},
+	// 	{
+	// 		questionText: 'How many Harry Potter books are there?',
+	// 		answerOptions: [
+	// 			{ answerText: '1', isCorrect: false },
+	// 			{ answerText: '4', isCorrect: false },
+	// 			{ answerText: '6', isCorrect: false },
+	// 			{ answerText: '7', isCorrect: true },
+	// 		],
+	// 	},
+	// ];
 
 	const [currentQuestion, setCurrentQuestion] = useState(0);
 
@@ -46,13 +46,14 @@ export default function App() {
 
 	const [score, setScore] = useState(0);
 
-	const handleAnswerButtonClick = (isCorrect) => {
-		if(isCorrect===true){
-			setScore(score + 1);
+	const handleCorrectAnswerClick = ({correct_answer}) => {
+		if(quesList[currentQuestion].correct_answer===true){
+			setScore(score + 5);
 		}
 
 		const nextQuestion = currentQuestion + 1;
-		if(nextQuestion < questions.length){
+		
+		if(nextQuestion < quesList.length){
 			setCurrentQuestion(nextQuestion);
 		}else{
 			setShowScore(true);
@@ -60,23 +61,65 @@ export default function App() {
 		
 	};
 
-	return (
+	const handleIncorrectAnswerClick = () =>{
+		if(quesList[currentQuestion].correct_answer===false){
+			setScore(score - 1);
+		}
+
+		const nextQuestion = currentQuestion + 1;
+		
+		if(nextQuestion < quesList.length){
+			setCurrentQuestion(nextQuestion);
+		}else{
+			setShowScore(true);
+		}
+	};
+
+	const URL = 'https://opentdb.com/api.php?amount=10';
+	
+	const [quesList,setQuesList] = useState([]);
+
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+        const fetchData = async () => {
+            try {
+                fetch(URL)
+                    .then((res) => res.json())
+                    .then((res) => {
+                        console.log(res.results);
+                        setQuesList(res.results);
+						setLoading(false);
+                    })
+            }
+            catch (error) {
+                console.log("error : ", error)
+            }
+        };
+
+        fetchData();
+    }, []);
+
+		return (
 		<div className='app'>
 		
 			{showScore ? (
-				<div className='score-section'>You scored {score} out of {questions.length}</div>
-			) : (
+				<div className='score-section'>You scored {score} out of 50</div>
+			) : 
+			( loading ? <div>Loading</div> :
 				<>
 					<div className='question-section'>
 						<div className='question-count'>
-							<span>Question {currentQuestion + 1}</span>/{questions.length}
+							<span>Question {currentQuestion + 1}</span>/{quesList.length}
 						</div>
-						<div className='question-text'>{questions[currentQuestion].questionText}</div>
+						<div className='question-text'>{quesList[currentQuestion].question}</div>
 					</div>
 					<div className='answer-section'>
-						{questions[currentQuestion].answerOptions.map((answerOptions)=>(
-						<button onClick={() => handleAnswerButtonClick(answerOptions.isCorrect)}>{answerOptions.answerText}</button>
+						{quesList[currentQuestion].incorrect_answers.map((incorrect_options, index)=>(						
+						<button key={index} onClick={() => handleIncorrectAnswerClick}>{incorrect_options}</button>								
 						))}
+
+						<button onClick={() => handleCorrectAnswerClick(quesList[currentQuestion].correct_answer)}>{quesList[currentQuestion].correct_answer}</button>
 					</div>
 				</>
 			)}
